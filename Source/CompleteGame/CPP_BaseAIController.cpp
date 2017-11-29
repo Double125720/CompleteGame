454c5d3e883542e25e60a7e2b48d7bb37fddfdae
Double125720 <Double125720@gmail.com>
2017-11-29 04:22:58 +0300

    Finally -_-

--- Source/CompleteGame/CPP_BaseAIController.cpp
+++ Source/CompleteGame/CPP_BaseAIController.cpp
@@ -9,33 +9,42 @@ ACPP_BaseAIController::ACPP_BaseAIController()
 
 bool ACPP_BaseAIController::Vision()
 {
-	TArray<AActor*> creatures = creatureManager->GetCreatureList();
-	for (int i = 0; i < creatures.Num() - 1; i++)
+	if (!Cast<ACPP_Creature>(this->GetPawn())->bIsUnderPlayerControl)
 	{
-		if ((Cast<ACPP_Creature>(creatures[i])->bIsUnderPlayerControl) && (Cast<ACPP_Creature>(creatures[i])->Team == 0))
+		TArray<AActor*> creatures = creatureManager->GetCreatureList();
+		for (int i = 0; i < creatures.Num() - 1; i++)
 		{
-			FVector vectToTarget = (this->GetPawn()->GetActorLocation() - creatures[i]->GetActorLocation());
-			if (FVector::Dist(this->GetPawn()->GetActorLocation(), vectToTarget) <= Cast<ACPP_Creature>(this->GetPawn())->VisionDistance)
+			if ((Cast<ACPP_Creature>(creatures[i])->bIsUnderPlayerControl) && (Cast<ACPP_Creature>(creatures[i])->Team == 0))
 			{
-				FVector normalDirVect = vectToTarget;
-				normalDirVect.Normalize();
-				float angleBetween = FMath::RadiansToDegrees(FMath::Acos(FVector::DotProduct(this->GetPawn()->GetActorForwardVector(), normalDirVect)));
-				if (FMath::RadiansToDegrees(FMath::Acos(FVector::DotProduct(this->GetPawn()->GetActorForwardVector(), normalDirVect))) <= Cast<ACPP_Creature>(this->GetPawn())->FOV)
+				DrawDebugLine(GetWorld(), this->GetPawn()->GetActorLocation(), (this->GetPawn()->GetActorLocation() + (this->GetPawn()->GetActorForwardVector()*250)), FColor::Red);
+				DrawDebugLine(GetWorld(), this->GetPawn()->GetActorLocation(), creatures[i]->GetActorLocation(), FColor::Green);
+				FVector vectToTarget = (creatures[i]->GetActorLocation() - this->GetPawn()->GetActorLocation());
+				if (FVector::Dist(this->GetPawn()->GetActorLocation(), vectToTarget) <= Cast<ACPP_Creature>(this->GetPawn())->VisionDistance)
 				{
-					ECollisionChannel CollisionChannel = ECC_Visibility;
-					FCollisionQueryParams TraceParams(FName(TEXT("VictoreCore Trace")), true, this->GetPawn());
-					TraceParams.bTraceComplex = true;
-					FHitResult HitOut(ForceInit);
-					if (GetWorld()->LineTraceSingleByChannel(HitOut, this->GetPawn()->GetActorLocation(), creatures[i]->GetActorLocation(), CollisionChannel, TraceParams))
+					FVector normalDirVect = vectToTarget;
+					normalDirVect.Normalize();
+					float angleBetween = FMath::RadiansToDegrees(FMath::Acos(FVector::DotProduct(this->GetPawn()->GetActorForwardVector(), normalDirVect)));
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Angle between: %f"), angleBetween));
+					if (FMath::RadiansToDegrees(FMath::Acos(FVector::DotProduct(this->GetPawn()->GetActorForwardVector(), normalDirVect))) <= Cast<ACPP_Creature>(this->GetPawn())->FOV)
 					{
-						DrawDebugLine(GetWorld(), this->GetPawn()->GetActorLocation(), creatures[i]->GetActorLocation(), FColor::Red);
-						if (HitOut.GetActor() == creatures[i])
+						GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("FOV is alright")));
+						ECollisionChannel CollisionChannel = ECC_Visibility;
+						FCollisionQueryParams TraceParams(FName(TEXT("VictoreCore Trace")), true, this->GetPawn());
+						TraceParams.bTraceComplex = true;
+						TraceParams.AddIgnoredActor(Cast<AActor>(this->GetPawn()));
+						
+						FHitResult HitOut(ForceInit);
+						if (GetWorld()->LineTraceSingleByChannel(HitOut, this->GetPawn()->GetActorLocation(), creatures[i]->GetActorLocation(), CollisionChannel, TraceParams))
 						{
-							GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("I see the playah")));
-							return true;
+							if (HitOut.GetActor() == creatures[i])
+							{
+								DrawDebugLine(GetWorld(), this->GetPawn()->GetActorLocation(), creatures[i]->GetActorLocation(), FColor::Blue, true);
+								GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("I see the playah")));
+								return true;
+							}
 						}
-					}
 
+					}
 				}
 			}
 		}
